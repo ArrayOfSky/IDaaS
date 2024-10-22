4cb8c4011b8835d389e60a6d5b0533418335f76c
Gaoyifeng <104308413+ArrayOfSky@users.noreply.github.com>
2024-10-22 21:55:26 +0800

    DDD架构完善

--- IDaaS-domain/src/main/java/com/gaoyifeng/IDaaS/domain/auth/service/ICodeService.java
+++ IDaaS-domain/src/main/java/com/gaoyifeng/IDaaS/domain/auth/service/ICodeService.java
@@ -1,4 +1,9 @@
 package com.gaoyifeng.IDaaS.domain.auth.service;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface ICodeService {
+
+    void getCode(String account, String type);
 }
--- IDaaS-trigger/src/main/java/com/gaoyifeng/IDaaS/trigger/http/CodeController.java
+++ IDaaS-trigger/src/main/java/com/gaoyifeng/IDaaS/trigger/http/CodeController.java
@@ -1,8 +1,13 @@
 package com.gaoyifeng.IDaaS.trigger.http;
 
 
+import com.gaoyifeng.IDaaS.domain.auth.service.ICodeService;
+import com.gaoyifeng.IDaaS.domain.auth.service.code.CodeService;
 import com.gaoyifeng.IDaaS.trigger.http.dto.CodeEntity;
 import com.gaoyifeng.IDaaS.trigger.http.dto.ValidEntity;
+import com.gaoyifeng.IDaaS.types.commom.Constants;
+import com.gaoyifeng.IDaaS.types.model.Response;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/code")
 public class CodeController {
 
+    @Resource
+    private ICodeService codeService;
 
     @PostMapping("/getCode")
-    public void getCode(@RequestBody CodeEntity codeEntity){
-
+    public Response<String> getCode(@RequestBody CodeEntity codeEntity){
+        String account = codeEntity.getAccount();
+        String type = codeEntity.getType();
+        codeService.getCode(account,type);
+        return Response.<String>builder()
+                .code(Constants.ResponseCode.SUCCESS.getCode())
+                .info(Constants.ResponseCode.SUCCESS.getInfo())
+                .data("")
+                .build();
     }
 
     @PostMapping("/validCode")
     public void validCode(@RequestBody ValidEntity validEntity){
-
+        validEntity.getAccount();
+        validEntity.getCode();
+        validEntity.getType();
     }
 
 }
